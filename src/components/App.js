e597b3c9462e240080be13a0e5943bf6b84b4523
Sophie <sophie.milash@yandex.ru>
2021-06-26 18:49:22 +0200

    feat: add current user context

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,32 @@ import EditAvatarPopup from './popups/EditAvatarPopup';
 import AddCardPopup from './popups/AddCardPopup';
 import ImagePopup from './popups/ImagePopup';
 import DeletionConfirmPopup from './popups/DeletionConfirmPopup';
-
+import CurrentUserContext from '../contexts/CurrentUserContext';
+import api from '../utils/Api';
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
   const [isAddCardPopupOpen, setIsAddCardPopupOpen] = React.useState(false);
   const [selectedCard, setSelectedCard] = React.useState(null);
+  const [currentUser, setCurrentUser] = React.useState({});
+  const [cards, setCards] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    Promise.all([
+      api.getUserInfo(),
+      api.getInitialCards()
+    ])
+      .then(([userData, cards]) => {
+        setCurrentUser(userData);
+        setCards(cards);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  }, []);
 
   function handleEditProfileClick() {
     setIsEditProfilePopupOpen(true);
@@ -40,14 +59,16 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Main onEditAvatar={handleEditProfileClick} onEditProfile={handleEditAvatarClick} onAddCard={handleAddCardClick} onCardClick={handleCardClick} />
-      <Footer />
-      <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} />
-      <EditAvatarPopup isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} />
-      <AddCardPopup isOpen={isAddCardPopupOpen} onClose={closeAllPopups} />
-      <ImagePopup card={selectedCard !== null && selectedCard} onClose={closeAllPopups} />
-      <DeletionConfirmPopup />
+      <CurrentUserContext.Provider value={currentUser}>
+        <Header />
+        <Main onEditAvatar={handleEditProfileClick} onEditProfile={handleEditAvatarClick} onAddCard={handleAddCardClick} onCardClick={handleCardClick} cards={cards} isLoading={isLoading} />
+        <Footer />
+        <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} />
+        <EditAvatarPopup isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} />
+        <AddCardPopup isOpen={isAddCardPopupOpen} onClose={closeAllPopups} />
+        <ImagePopup card={selectedCard !== null && selectedCard} onClose={closeAllPopups} />
+        <DeletionConfirmPopup />
+      </CurrentUserContext.Provider>
     </>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,44 @@
 import React from 'react';
 import Loader from './Loader';
 import Card from './Card';
-import api from '../utils/Api';
+import CurrentUserContext from '../contexts/CurrentUserContext';
 
 function Main(props) {
-  const [userName, setuserName] = React.useState('');
-  const [userDescription, setUserDescription] = React.useState('');
-  const [userAvatar, setUserAvatar] = React.useState('');
-  const [cards, setCards] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(false);
+  const currentUser = React.useContext(CurrentUserContext);
 
-  React.useEffect(() => {
-    setIsLoading(true);
+  // function handleCardLike(card) {
+  //   const isLiked = card.likes.some(i => i._id === currentUser._id);
 
-    Promise.all([
-      api.getUserInfo(),
-      api.getInitialCards()
-    ])
-      .then(([userData, cards]) => {
-        setuserName(userData.name);
-        setUserDescription(userData.about);
-        setUserAvatar(userData.avatar);
-        setCards(cards);
-      })
-      .catch((err) => console.log(err))
-      .finally(() => setIsLoading(false));
-  }, []);
+  //   api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
+  //     setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+  //   });
+  // }
+
+  // function handleCardDelete() {
+
+  // }
 
   return (
     <main className="content">
-      {isLoading && (<Loader />)}
+      {props.isLoading && (<Loader />)}
 
-      <section className={`profile page__section page__section_place_profile ${isLoading && "page__section_hidden"}`}>
+      <section className={`profile page__section page__section_place_profile ${props.isLoading && "page__section_hidden"}`}>
         <div className="profile__avatar-wrap">
-          <img src={userAvatar} alt="аватар" className="profile__avatar" />
+          <img src={currentUser.avatar} alt="аватар" className="profile__avatar" />
           <button type="button" aria-label="Редактировать" title="Редактировать" className="button profile__avatar-edit" onClick={props.onEditAvatar}></button>
         </div>
         <div className="profile__info">
-          <h1 className="profile__name">{userName}</h1>
+          <h1 className="profile__name">{currentUser.name}</h1>
           <button type="button" aria-label="Редактировать" title="Редактировать" className="button button_action_edit profile__info-button" onClick={props.onEditProfile}></button>
-          <p className="profile__activity">{userDescription}</p>
+          <p className="profile__activity">{currentUser.about}</p>
         </div>
         <button type="button" aria-label="Добавить" title="Добавить" className="button button_action_add-card profile__button" onClick={props.onAddCard}></button>
       </section>
 
-      <section className={`cards page__section page__section_place_cards ${isLoading && "page__section_hidden"}`}>
-        {cards.map((card) => (
+      <section className={`cards page__section page__section_place_cards ${props.isLoading && "page__section_hidden"}`}>
+        {props.cards.map((card) => (
          <Card card={card} key={card._id} onCardClick={props.onCardClick} />
+        //  onCardLike={handleCardLike}
         ))}
       </section>
     </main>
